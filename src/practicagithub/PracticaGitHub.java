e350d7effb68f298f7d6940fe4999f6e0d563fea
st_da <st_da@192.168.0.10>
2018-09-07 23:07:28 -0300

    probando primer repositorio, actualizando

--- src/practicagithub/PracticaGitHub.java
+++ src/practicagithub/PracticaGitHub.java
@@ -6,6 +6,7 @@ public class PracticaGitHub {
     public static void main(String[] args) {
         
         System.out.println("Primer repositorio");
+        System.out.println("Actualizando");
     }
     
 }
